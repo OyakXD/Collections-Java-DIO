25900a86fbf0eac9f8e6fc868dc715699cba5ff7
OyakXD <kayovieira157@gmail.com>
2024-04-30 12:10:09 -0300

    feat: Implemeting to-do list logic and testing

---
+++ src/ListaTarefas.java
@@ -0,0 +1,51 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefas {
+    // ATRIBUTO
+
+    private List<Tarefa> tarefalist;
+
+    // CONSTRUTOR
+
+    public ListaTarefas() {
+        this.tarefalist = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao) {
+        this.tarefalist.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao) {
+        List<Tarefa> tarefasARemover = new ArrayList<>();
+        for (Tarefa tarefa : this.tarefalist) {
+            if (tarefa.getDescricao().equalsIgnoreCase(descricao)) {
+                tarefasARemover.add(tarefa);
+            }
+        }
+        tarefalist.removeAll(tarefasARemover);
+    }
+
+    public int obterNumeroTotalTarefas() {
+        return this.tarefalist.size();
+    }
+
+    public void obterDescricaoTarefas() {
+        System.out.println(tarefalist);
+    }
+
+    public static void main(String[] args) {
+        ListaTarefas listaTarefas = new ListaTarefas();
+        listaTarefas.adicionarTarefa("Estudar Java");
+        listaTarefas.adicionarTarefa("Estudar Python");
+        listaTarefas.adicionarTarefa("Estudar JavaScript");
+        listaTarefas.adicionarTarefa("Estudar C++");
+        listaTarefas.adicionarTarefa("Estudar C#");
+
+        listaTarefas.obterNumeroTotalTarefas();
+
+    }
+
+}
---
+++ src/Tarefa.java
@@ -0,0 +1,14 @@
+package src;
+
+public class Tarefa {
+
+    public String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
